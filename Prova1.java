94d292e98d98e53686efcb73bde481f2a9e5dc8c
Felipe Rosa Ferreira <82189808+felipeferreira09@users.noreply.github.com>
2021-09-23 16:12:16 -0300

    Update Prova1.java

--- Prova1.java
+++ Prova1.java
@@ -1,25 +1,36 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Prova {
+
+public class Prova1 {
 
     public static void main(String[] args)
     {
         
+
+
         //botar um vetor/array para enumerar e cadastrar as questões.
 
         Question third = new Question();
         third.setText("Quantos anos eu tenho?");
         third.setAnswer("19");
-        
-        
-       
         presentQuestion(third);
+        
+
+
+
+        ArrayList<String> Questoes = new ArrayList<String>();
+        //Questoes.add(third);
+       
     }
+    
+    
     
     public static void presentQuestion(Question q)
     {
         q.display();
-        System.out.print("Your answer: ");
+        System.out.print("Sua resposta: ");
         Scanner in = new Scanner(System.in);
         String response = in.nextLine();
         System.out.println(q.checkAnswer(response));
